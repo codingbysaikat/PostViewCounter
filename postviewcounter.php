8ac3f45ba6efa3753a5e5a5d214ca993c12f6a68
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-11-06 01:08:45 +0600

    up to date

--- postviewcounter.php
+++ postviewcounter.php
@@ -1,13 +1,15 @@
 <?php 
 /*
 Plugin Name: Posts View Counter
-Plugin URI: 
+Plugin URI: https://github.com/codingbysaikat/WordCounter.git
 Description: Count your post view and Display it 
 Version: 1.0
 Author: saikat mondal
 Author URI: https://saikatmondal.con
 License: GPLv2 or later
-Text Domain: postview
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+Update URI:        https://example.com/my-plugin/
+Text Domain: countview
 Domain Path: /languages/
 */
 
@@ -18,49 +20,54 @@ register_activation_hook(__FILE__,"wordcount_activation_hook");
 function wordcount_deactivation_hook(){}
 register_deactivation_hook(__FILE__,"wordcount_deactivation_hook");
 */
+function wordcount_load_textdomain() {
+    load_plugin_textdomain( 'countview', false, dirname( __FILE__ ) . "/languages" );
+}
 
-$postview_postID = get_the_ID();
+
+
+
+    $countview_current_post = get_the_ID();
 
 
 // Set Post View
-function setPostViews(){
-    global $postview_postID;
-    $cookie_name = 'viewCount' . $postview_postID;
-    $cookie_value = $postview_postID;
-    setcookie($cookie_name, $cookie_value, time() + 60);
+function countview_setPostViews($postID){
+    $cookie_name = 'viewCount' . $postID;
+    $cookie_value = $postID;
+    setcookie($cookie_name, $cookie_value, time() + 0);
     $count_key = 'post_views_count';
-    $count = get_post_meta($postview_postID, $count_key, true);
+    $count = get_post_meta($postID, $count_key, true);
     if ($count == '') {
         $count = 0;
-        delete_post_meta($postview_postID, $count_key);
-        add_post_meta($postview_postID, $count_key, '0');
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
     } else {
         if (!isset($_COOKIE[$cookie_name])) {
             $count++;
-            update_post_meta($postview_postID, $count_key, $count);
+            update_post_meta($postID, $count_key, $count);
         }
     }
 }
-setPostViews();
+countview_setPostViews($countview_current_post );
 
 // Get Post View
-function getPostViews(){
-    global $postview_postID;
+function countview_getPostViews($postID){
     $count_key = 'post_views_count';
-    $count = get_post_meta($postview_postID, $count_key, true);
+    $count = get_post_meta($postID, $count_key, true);
     if ($count == '') {
-        delete_post_meta($postview_postID, $count_key);
-        add_post_meta($postview_postID, $count_key, '0');
+        delete_post_meta($postID, $count_key);
+        add_post_meta($postID, $count_key, '0');
         return "0 View";
     }
     return $count . ' Views';
 }
-$postview_count = getPostViews();
+$countview_count = countview_getPostViews($countview_current_post);
+
 
 // Display the post view by the short code
 function postview_display($attr,$content){
-    global $postview_postID;
+    global $countview_count;
 
- return $postview_postID;
+ return $countview_count;
 }
 add_shortcode('postview','postview_display');
